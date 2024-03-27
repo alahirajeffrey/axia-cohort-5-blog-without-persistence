17835bce13e5a9e2f7ddb056cacd41fc1c42a95e
alahirajeffrey <alahirajeffrey@gmail.com>
2024-03-27 20:37:00 +0100

    made changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 // import express
 const express = require("express");
+const HttpStatus = require("http-status-codes");
 
 // initialize express
 const app = express();
+
+// parse json objects
+app.use(express.json());
 
 // attaching homepage route
 app.get("/homepage", (req, res) => {
@@ -14,6 +18,16 @@ app.get("/test", (request, response) => {
   response.status(200).json({ message: "you have hit the test page" });
 });
 
+app.post("/posting-somethig", (request, response) => {
+  //   console.log(request);
+  //   console.log(response);
+
+  const payload = request.body;
+  console.log(payload);
+  response.status(HttpStatus.StatusCodes.OK).json(payload);
+});
+
+// starting the express server
 app.listen(3000, () => {
   console.log("server is running");
 });
